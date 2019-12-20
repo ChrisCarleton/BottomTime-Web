b975815a8afa9e78691495e0258f8088c92827f1
Chris Carleton <mrchriscarleton@gmail.com>
2019-12-20 16:19:58 -0500

    Wired up Complete Registration page for newly created accounts.

--- web/home/components/app.jsx
+++ web/home/components/app.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import connectToStores from 'alt-utils/lib/connectToStores';
+import CurrentUserStore from '../../users/stores/current-user-store';
 import ErrorBar from '../../components/error-bar';
 import LoadingSpinner from '../../components/loading-spinner';
 import NavBar from '../../components/navbar';
+import PropTypes from 'prop-types';
 import React, { lazy, Suspense } from 'react';
 
 const ChangePassword = lazy(() => import('../../users/components/change-password'));
+const CompleteRegistration = lazy(() => import('../../users/components/complete-registration'));
 const DiveSite = lazy(() => import('../../dive-sites/components/dive-site'));
 const DiveSites = lazy(() => import('../../dive-sites/components/dive-sites'));
 const EmailTaken = lazy(() => import('./email-taken'));
@@ -22,7 +26,46 @@ const SignUp = lazy(() => import('../../users/components/sign-up'));
 const spinner = <LoadingSpinner message="Loading page..." />;
 
 class App extends React.Component {
+	static getStores() {
+		return [ CurrentUserStore ];
+	}
+
+	static getPropsFromStores() {
+		return CurrentUserStore.getState();
+	}
+
+	renderRouter() {
+		return (
+			<Switch>
+				<Route path="/" exact component={ Home } />
+				<Route path="/signup" exact component={ SignUp } />
+				<Route path="/login" exact component={ Login } />
+				<Route path="/diveSites" exact component={ DiveSites } />
+				<Route path="/diveSites/new" exact component={ DiveSite } />
+				<Route path="/diveSites/:siteId" exact component={ DiveSite } />
+				<Route path="/friends" exact component={ Friends } />
+				<Route path="/friendRequests" exact component={ Friends } />
+				<Route path="/changePassword" exact component={ ChangePassword } />
+				<Route path="/profile" exact component={ Profile } />
+				<Route path="/profile/:username" exact component={ Profile } />
+				<Route path="/logs" exact component={ LogsList } />
+				<Route path="/logs/:username" exact component={ LogsList } />
+				<Route path="/logs/:username/new" exact component={ LogEntry } />
+				<Route path="/logs/:username/:logId" exact component={ LogEntry } />
+				<Route path="/forbidden" exact component={ Forbidden } />
+				<Route path="/notFound" exact component={ NotFound } />
+				<Route path="/emailTaken/:provider" exact component={ EmailTaken } />
+				<Route path="/serverError" exact component={ ServerError } />
+				<Route path="*" component={ NotFound } />
+			</Switch>
+		);
+	}
+
 	render() {
+		const pageContent = this.props.currentUser.isRegistrationIncomplete
+			? <CompleteRegistration />
+			: this.renderRouter();
+
 		return (
 			<Router>
 				<div>
@@ -30,28 +73,7 @@ class App extends React.Component {
 					<ErrorBar />
 					<div id="main-section" className="container">
 						<Suspense fallback={ spinner }>
-							<Switch>
-								<Route path="/" exact component={ Home } />
-								<Route path="/signup" exact component={ SignUp } />
-								<Route path="/login" exact component={ Login } />
-								<Route path="/diveSites" exact component={ DiveSites } />
-								<Route path="/diveSites/new" exact component={ DiveSite } />
-								<Route path="/diveSites/:siteId" exact component={ DiveSite } />
-								<Route path="/friends" exact component={ Friends } />
-								<Route path="/friendRequests" exact component={ Friends } />
-								<Route path="/changePassword" exact component={ ChangePassword } />
-								<Route path="/profile" exact component={ Profile } />
-								<Route path="/profile/:username" exact component={ Profile } />
-								<Route path="/logs" exact component={ LogsList } />
-								<Route path="/logs/:username" exact component={ LogsList } />
-								<Route path="/logs/:username/new" exact component={ LogEntry } />
-								<Route path="/logs/:username/:logId" exact component={ LogEntry } />
-								<Route path="/forbidden" exact component={ Forbidden } />
-								<Route path="/notFound" exact component={ NotFound } />
-								<Route path="/emailTaken/:provider" exact component={ EmailTaken } />
-								<Route path="/serverError" exact component={ ServerError } />
-								<Route path="*" component={ NotFound } />
-							</Switch>
+							{ pageContent }
 						</Suspense>
 						<hr />
 						<p className="text-right"><small><em>Copyright &copy; Chris Carleton, 2019</em></small></p>
@@ -61,4 +83,8 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+App.propTypes = {
+	currentUser: PropTypes.object.isRequired
+};
+
+export default connectToStores(App);
