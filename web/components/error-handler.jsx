3a100c9a3f105ac9e494d29b11a04867103b9de2
Chris Carleton <mrchriscarleton@gmail.com>
2019-12-28 18:05:14 -0500

    Error handler HOF now works independently of handleError function.

--- web/components/error-handler.jsx
+++ web/components/error-handler.jsx
@@ -1,28 +1,94 @@
-import handleError from '../handle-error';
-import PropTypes from 'prop-types';
+/* eslint no-console: 0 */
+
+import CurrentUserActions from '../users/actions/current-user-actions';
+import ErrorActions from '../actions/error-actions';
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+
+import Forbidden from '../home/components/forbidden';
+import Login from '../users/components/login';
+import NotFound from '../home/components/not-found';
+import ServerError from '../home/components/server-error';
+
+const NormalStatus = 200;
+const BadRequestStatus = 400;
+const UnauthorizedStatus = 401;
+const ForbiddenStatus = 403;
+const NotFoundStatus = 404;
+const ServerErrorStatus = 500;
 
 export default function (page) {
 	class ErrorHandler extends React.Component {
 		constructor(props) {
 			super(props);
+
 			this.PageComponent = page;
+			this.state = { status: NormalStatus };
+
 			this.onErrorResponse = this.onErrorResponse.bind(this);
 		}
 
 		onErrorResponse(err) {
-			handleError(err, this.props.history);
+			if (err.response) {
+				switch (err.response.status) {
+				case BadRequestStatus:
+					console.error(
+						'Validation Error:',
+						err.response.body.details.isJoi
+							? err.response.body.details.details
+							: err.response.body
+					);
+					ErrorActions.showError(
+						'Your request failed',
+						'There was a problem with your something you input. Check your form fields and try again.'
+					);
+					this.setState({ status: NormalStatus });
+					break;
+
+				case UnauthorizedStatus:
+					CurrentUserActions.logout();
+					this.setState({ status: UnauthorizedStatus });
+					break;
+
+				case ForbiddenStatus:
+					this.setState({ status: ForbiddenStatus });
+					break;
+
+				case NotFoundStatus:
+					this.setState({ status: NotFoundStatus });
+					break;
+
+				case ServerErrorStatus:
+					console.error('Server error occurred. Log ID:', err.response.body.logId);
+					this.setState({ status: ServerErrorStatus });
+					break;
+
+				default:
+					console.error('Unknown error response:', JSON.stringify(err.response.body));
+					this.setState({ status: NormalStatus });
+					break;
+				}
+			}
 		}
 
 		render() {
-			return <this.PageComponent handleError={ this.onErrorResponse } { ...this.props } />;
+			switch (this.state.status) {
+			case UnauthorizedStatus:
+				return <Login />;
+
+			case ForbiddenStatus:
+				return <Forbidden />;
+
+			case NotFoundStatus:
+				return <NotFound />;
+
+			case ServerErrorStatus:
+				return <ServerError />;
+
+			default:
+				return <this.PageComponent handleError={ this.onErrorResponse } { ...this.props } />;
+			}
 		}
 	}
 
-	ErrorHandler.propTypes = {
-		history: PropTypes.object.isRequired
-	};
-
-	return withRouter(ErrorHandler);
+	return ErrorHandler;
 }
